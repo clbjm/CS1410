31be9f75e1fb580273803692d247ec0249f0d063
spikesynergy <odaykevin@gmail.com>
2014-12-02 22:57:07 -0700

    Update FileException.h

--- Lab_10/Lab_10/FileException.h
+++ Lab_10/Lab_10/FileException.h
@@ -4,7 +4,7 @@
 // Version:		1.0
 // Author:		Kevin S. O'Day
 // Course:		CS 1410 Section 002
-// Instructor:	Dennis Fairclough
+// Instructor:		Dennis Fairclough
 // Project:		Proj_09
 // Date:		November 2014
 // Date:		11/26/14 9:59 AM
